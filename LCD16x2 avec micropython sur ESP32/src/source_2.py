25aef3c326eedba0667914cd89bf7670df3f9b86
LONTCHEDJI Roméo <128220912+romeolontchedji@users.noreply.github.com>
2024-11-13 04:34:32 +0100

    Update source_2.py

--- LCD16x2 avec micropython sur ESP32/src/source_2.py
+++ LCD16x2 avec micropython sur ESP32/src/source_2.py
@@ -1,19 +1,27 @@
 from i2c_lcd import I2cLcd
-from machine import I2C,Pin
+from machine import SoftI2C, Pin
 import time
 
+# Configuration de l'I2C et de l'écran LCD
 address = 0x27
-i2c = I2C(scl=Pin(22),sda=Pin(21),freq=400000)
-lcd = I2cLcd(i2c,address,2,16)
+i2c = SoftI2C(scl=Pin(22), sda=Pin(21), freq=400000)
+lcd = I2cLcd(i2c, address, 2, 16)
 
-lcd.display_on() #allume ecran d'affichage
-lcd.backlight_on() #retro-eclairage
+lcd.display_on()  # Allume l'écran d'affichage
+lcd.backlight_on()  # Active le rétroéclairage
 
-for i in range(2):
-    for j in range(16):
-        lcd.move_to(0,1)
-        lcd.putstr("BONJOUR TOUT LE MONDE")
-        time.sleep_ms(500)
-        lcd.clear()
+# Message à afficher
+message = "ABONNEZ VOUS A LA CHAINE SVP "
+
+# Défilement du message
+while True:
+    for i in range(len(message) - 15):
+        lcd.move_to(0, 0)  # Déplacement sur la première ligne
+        # Affichage d'une sous-chaîne du message de longueur 16 caractères
+        lcd.putstr(message[i:i + 16])
+        time.sleep(1)  # Pause pour l'effet de défilement
+
+
+
 
 
